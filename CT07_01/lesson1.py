554bc46e63688aeeb40511ea10b1ba4070db72af
JaydenWongZhen <taymeister@gmail.com>
2026-01-08 10:56:23 +0000

    i have completed the lesson 1

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,36 @@
-print("Hello from lesson 1")
+# print("Drywall")
+# hiddenpasscode = "sigmablahblah"
+# inputcode = input("INPUT PASSCODE / ")
+# if inputcode == hiddenpasscode:
+#     print("ACCESS GRANTED")
+# else:
+#     print("ERROR: PASSWORD WRONG, TRY AGAIN")
 
+# n1 = int(input("insert number 1 here "))
+# n2 = int(input("inert number 2 here "))
+# ans = n1+n2
+# print(str(n1) + "+" + str(n2) + " is " + str(ans))
+
+# make 3 lists, glass items, plastic items, paper items
+# btw lists kinda like this: listname=[listitem1,listitem2]
+
+#if item is one of the listed items, it will sense it using a sensor 
+# if item does not fall into any category, robot says it is unable to recycle item
+# it will put the item into the bin named by each list
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " , student_name , " is: " , average_score)
+# a=10
+# for i in range(9):
+#     a=a-1
+#     print(str(a))
+# print("happy 2026 cny")
