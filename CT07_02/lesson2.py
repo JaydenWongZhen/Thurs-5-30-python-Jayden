8c115b25373b2a889d29b63c11ba1403922bc15e
JaydenWongZhen <taymeister@gmail.com>
2026-01-15 10:55:17 +0000

    end o leson 2

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,2 +1,62 @@
-print("Hello from lesson 2")
+# print("hi gfngfiug")
+# for var in range(100):
+#     print("mango")
+# for e in "mango":
+#     print("WE HAVE" , e)
+# print("ITS A FRICKIN MANGO")
+# print("hi")
 
+# for jndsfh in range(5):
+#     print("mango mango mango")
+#     for opangangnasta in range(2):
+#         print("banana banana banana to the power of 3")
+#     print("poop to the power of 3")
+
+# print("i say my farewells to you")
+
+# for njdfs in range(0,21):
+#     print(njdfs)
+# for njfv in range(1,31):
+#     print(njfv)
+# for nh in range(2,25,2):
+#     print(nh)
+# m = 0
+# while m < 5:
+#     print("domer")
+#     m += 1
+# c = 1
+# while True:
+#     c += 1 
+#     print(str(c))
+#     if c == 6:
+#         break
+# a = 0
+# while a < 10:
+#     a += 1
+#     print(str(a))
+#     if a == 5:
+#         print("OBJECTION!")
+#         break
+# else:
+#     print("the 10 has reached counter")
+# topings = ""
+# ans = ""
+# print("Henlo, Welcome to Papa Farts")
+# while ans != "end":
+
+#     ans = input("What toppings do you want? ")
+#     if ans.lower() == "pineapple":
+#         print("No!")
+#         topings += ""
+#     elif ans != "end":
+#         topings += ans + ", "
+# else:
+#     print("Pizza with" , topings ,  "is done.")
+
+qestio = "How many dots are on a pair of dice? "
+ansr = "42"
+inut = input(qestio)
+while ansr != inut:
+    print("no stupid")
+    inut = input(qestio)
+print("correct!!!!!!!!!!!!!")
