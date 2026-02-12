8a5657c38b07e8e505c25a9c8e8204bbcb7649a7
JaydenWongZhen <taymeister@gmail.com>
2026-02-12 10:50:41 +0000

    lesson 5 complete and lesson 6 almst

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -15,10 +15,34 @@ stduns=[]
 # print("most makrs trohphy go to someone with the marks: " , max(stduns))
 # print("the most outrageous loser goes to someone with the cringe marks: " , min(stduns))
 
-namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
-            "Sophia", "Lucas", "Mia", "Aiden"
-            ]
-heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
-tal=max(heightlist)
-conn=heightlist.index(tal)
-print(namelist[conn] , " is ze tallest student at ", tal , " cm")
+# namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
+#             "Sophia", "Lucas", "Mia", "Aiden"
+#             ]
+# heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+# tal=max(heightlist)
+# conn=heightlist.index(tal)
+# print(namelist[conn] , " is ze tallest student at ", tal , " cm")
+
+# pokemons = [
+#     "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
+#     "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
+#     "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
+#     "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
+#     "Electabuzz"
+# ]
+
+# powers = [
+#     550, 84, 49, 48, 45,
+#     45, 52, 55, 110, 110,
+#     85, 65, 134, 130, 110,
+#     50, 125, 65, 110, 83
+# ]
+# one = random.choice(pokemons)
+# two = random.choice(pokemons)
+# print("p1: ", one , " p2: " , two)
+# if powers[pokemons.index(one)]>powers[pokemons.index(two)]:
+#     print("player 1 win")
+# elif powers[pokemons.index(one)]<powers[pokemons.index(two)]:
+#     print("player 2 win")
+# else:
+#     print("hm a draw")
--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,2 +1,38 @@
-print("Hello from lesson 6")
+# print("ass")
 
+# contacts = []
+# contact1 = ["John", 98453126, "john@gmail.com"]
+# contact2 = ["Adam", 93029102, "adam@gmail.com"]
+# contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+
+# contacts.append(contact1)
+# contacts.append(contact2)
+# contacts.append(contact3)
+
+# print(contacts)
+# for name in contacts:
+# #     for val in name:
+# #         print(val)
+# #     print()
+#     name,phonnum,email = name
+#     print(name,phonnum,email)
+
+# students = [
+#     ["Olivia", "F"], ["Noah", "M"], ["Emma", "F"],
+#     ["Liam", "M"], ["Ava", "F"], ["Ethan", "M"],
+#     ["Sophia", "F"], ["Lucas", "M"], ["Mia", "F"],
+#     ["Aiden", "M"], ["Isabella", "F"], ["Jackson", "M"],
+#     ["Amelia", "F"], ["Logan", "M"], ["Lily", "F"]
+# ]
+
+# boys=[]
+# femalesubject=[]
+# for something in students:
+#     name,gender = something
+#     if gender == "F":
+#         femalesubject.append(name)
+#     elif gender == "M":
+#         boys.append(name)
+#     print(name,gender)
+# print("boi: ",boys)
+# print("baby dispeners: " , femalesubject)
