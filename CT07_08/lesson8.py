6be53d69498f71636b08c3847822804e48a18a3a
JaydenWongZhen <taymeister@gmail.com>
2026-03-26 11:05:09 +0000

    leson compensation complet and also som stuff

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,1 +1,19 @@
 print("i'm not paid enough to make these into a joke.")
+# sentence = "python makes coding fun"
+# w1=sentence[0:6]
+# print(w1)
+# w2=sentence[7:12]
+# print(w2)
+# p1=sentence[13:]
+# print(p1)
+# sw=sentence[13:19:2]
+# print(sw)
+# s="Singapore"
+# a=s[0:4]
+# print(a)
+# b=s[3:6]
+# print(b)
+# c=s[6:]
+# print(c)
+# d=s[0:10:2]
+# print(d)
--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,2 +1,20 @@
-print("Hello from lesson 9")
+# correct=False
+# ridel=input("what has to be broken beofre you can use it? ")
+# ridelow=ridel.lower()
+# if "egg" in ridelow:
+#     correct = True
+# if correct == True:
+#     print("yes you correct it egg")
+# else:
+#     print("no u worng it eg")
+# import turtle
+# window = turtle.Screen()
+# window.setup(width=200,height=200)
+# artist=turtle.Turtle()
+# artist.pendown()
+# artist.color("teal")
+# artist.left(90)
+# artist.forward(50)
 
+
+# window.mainloop()
